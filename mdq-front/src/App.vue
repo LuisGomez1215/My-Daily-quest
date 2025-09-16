8d810c4e0edcb2736eb3f912bc59fd8aec8fc4ef
akryaa <pablisquiv@gmail.com>
2025-09-15 22:24:33 -0300

    fix

---
+++ mdq-front/src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { IonApp, IonHeader, IonToolbar, IonTitle, IonContent, IonRouterOutlet, IonFooter } from '@ionic/vue'
+</script>
+
+<template>
+  <ion-router-outlet />
+  <IonApp>
+    <!-- Top Navbar -->
+    <IonHeader>
+      <IonToolbar style="--background: #f08897;">
+        <IonTitle>home</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+
+    <!-- Page Content -->
+    <IonContent fullscreen>
+      <!-- Messages (placeholder) -->
+      <div id="messages" class="ion-padding">
+        <!-- Replace with a Vue messages component later -->
+      </div>
+
+      <!-- Page Outlet (like {% block content %}) -->
+      <IonRouterOutlet />
+    </IonContent>
+
+    <!-- Bottom Navbar -->
+    <IonFooter>
+      <IonToolbar style="--background: #f08897;">
+        <IonTitle>Footer Nav</IonTitle>
+      </IonToolbar>
+    </IonFooter>
+  </IonApp>
+</template>
+
+<style>
+/* Optional: bring over styles from /public/css/main.css if needed */
+</style>
---
+++ mdq-front/src/views/TabsPage.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { IonTabs, IonRouterOutlet, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/vue'
+import { home, person, settings } from 'ionicons/icons'
+</script>
+
+<template>
+  <IonTabs>
+    <IonRouterOutlet />
+
+    <!-- Default Ionic Bottom Tabs -->
+    <IonTabBar slot="bottom">
+      <IonTabButton tab="tab1" href="/tabs/tab1">
+        <IonIcon :icon="home" />
+        <IonLabel>Home</IonLabel>
+      </IonTabButton>
+
+      <IonTabButton tab="tab2" href="/tabs/tab2">
+        <IonIcon :icon="person" />
+        <IonLabel>Profile</IonLabel>
+      </IonTabButton>
+
+      <IonTabButton tab="tab3" href="/tabs/tab3">
+        <IonIcon :icon="settings" />
+        <IonLabel>Settings</IonLabel>
+      </IonTabButton>
+    </IonTabBar>
+  </IonTabs>
+</template>
